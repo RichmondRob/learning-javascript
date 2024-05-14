35d95ad146b7429d4cee744c6a3b1916a5b466fc
Rob Tibbetts <robctibbetts@gmail.com>
2024-05-14 14:27:14 -0700

    Files from js lessons

---
+++ classes2.js
@@ -0,0 +1,13 @@
+const colors = require('colors')
+
+class Dog 
+{
+    constructor() {}
+
+    woof()
+    {
+        console.log('Woof!'.red)
+    }
+}
+
+module.exports = Dog
---
+++ loops.js
@@ -0,0 +1,18 @@
+// FOR LOOP
+
+// First part creates a counter variable
+// Second parts sets the condition in which the loop will execute
+// Third part increments the counter variable
+
+for(
+    x = 1;
+    x < 5;
+    x++ ) {
+       console.log("x is " + x)
+    }
+
+    let y = "foo"
+    for(x = 1; x < 5; x++){
+        y = y + "bar"
+        console.log(y)
+    }
